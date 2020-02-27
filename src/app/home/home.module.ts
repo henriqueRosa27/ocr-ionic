5f56eb57098555c03777d62a21e47ea7b7e503ee
Henrique Rosa <henrique.rosa@meta.com.br>
2020-02-27 16:15:26 -0300

    Adicionado funcionalidade base de abrir camera a abrir galeria

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { Camera } from '@ionic-native/camera/ngx';
 
 import { HomePage } from './home.page';
 
@@ -18,6 +19,7 @@ import { HomePage } from './home.page';
       }
     ])
   ],
+  providers: [Camera],
   declarations: [HomePage]
 })
 export class HomePageModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
+import { Camera, PictureSourceType } from '@ionic-native/camera/ngx';
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,47 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  selectedImage: string;
+  imageText: string;
 
+  constructor( public actionSheetController: ActionSheetController, private camera: Camera) {}
+
+  async selectSource() {    
+    const actionSheet = await this.actionSheetController.create({
+      header: 'Modo captura Foto',
+      buttons: [{
+        text: 'Galeria',
+        icon: 'albums',
+        handler: () => {
+          this.getPicture(this.camera.PictureSourceType.PHOTOLIBRARY);
+        }
+      }, 
+      {
+        text: 'Câmera',
+        icon: 'camera',
+        handler: () => {
+          this.getPicture(this.camera.PictureSourceType.CAMERA);
+        }
+      }, 
+      {
+        text: 'Cancelar',
+        icon: 'close-circle',
+        role: 'cancel'
+      }]
+    });
+    await actionSheet.present();
+  }
+
+  getPicture(sourceType: PictureSourceType) {
+    this.camera.getPicture({
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      sourceType: sourceType,
+      allowEdit: true,
+      saveToPhotoAlbum: false,
+      correctOrientation: true
+    }).then((imageData) => {
+      this.selectedImage = `data:image/jpeg;base64,${imageData}`;
+    });
+  }
 }
